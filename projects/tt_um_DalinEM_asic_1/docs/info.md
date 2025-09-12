36a773873cbfad3fd47933a3c1f8814c89e717de
TinyTapeoutBot <139130078+TinyTapeoutBot@users.noreply.github.com>
2025-09-12 05:25:22 +0300

    feat: update project tt_um_DalinEM_asic_1 from DalinEM-lab/tt_um_DalinEM_asic_1
    Commit: 47da0cf17548619a964a990dd707b67a1f324d51
    Workflow: https://github.com/DalinEM-lab/tt_um_DalinEM_asic_1/actions/runs/17570973107

---
+++ projects/tt_um_DalinEM_asic_1/docs/info.md
@@ -0,0 +1,20 @@
+<!---
+
+This file is used to generate your project datasheet. Please fill in the information below and delete any unused
+sections.
+
+You can also include images in this folder and reference them in the markdown. Each image must be less than
+512 kb in size, and the combined size of all images must be less than 1 MB.
+-->
+
+## How it works
+
+The device will compare the threshold voltage and generate current
+
+## How to test
+
+Apply input signals and measure the output current
+
+## External hardware
+
+Function generator and oscilloscope
---
+++ projects/tt_um_DalinEM_asic_1/tt_um_DalinEM_asic_1.v
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2024 Your Name
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+`default_nettype none
+
+module tt_um_DalinEM_asic_1 (
+    input  wire       VGND,
+    input  wire       VDPWR,    // 1.8v power supply
+    input  wire       VAPWR,    // 3.3v power supply
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    inout  wire [7:0] ua,       // Analog pins, only ua[5:0] can be used
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
+
+
+VCR subckt1(.vcc(VAPWR),.vss(VGND),.out(ua[0]),.vsen(ua[1]),.Vled(ua[2]));
+     latch subckt2(.VDD(VAPWR),.VSS(VGND),.S(ua[5]));
+     COMP_2 subckt_3(.vcc(VAPWR),.vss(VGND));
+     BIAS_1 subckt_4(.vcc(VAPWR),.vss(VGND));
+     delay_1 subckt_5(.vcc(VAPWR),.vss(VGND));
+     Timming subckt_6(.vcc(VAPWR),.vss(VGND));
+     MUX_1 subckt_7(.VDD(VAPWR),.VSS(VGND),.S0(ua[4]),.S1(ua[3]);
+
+assign uo_out[0] = VGND;
+    assign uo_out[1] = VGND;
+    assign uo_out[2] = VGND;
+    assign uo_out[3] = VGND;
+    assign uo_out[4] = VGND;
+    assign uo_out[5] = VGND;
+    assign uo_out[6] = VGND;
+    assign uo_out[7] = VGND;
+
+    assign uio_out[0] = VGND;
+    assign uio_out[1] = VGND;
+    assign uio_out[2] = VGND;
+    assign uio_out[3] = VGND;
+    assign uio_out[4] = VGND;
+    assign uio_out[5] = VGND;
+    assign uio_out[6] = VGND;
+    assign uio_out[7] = VGND;
+
+    assign uio_oe[0] = VGND;
+    assign uio_oe[1] = VGND;
+    assign uio_oe[2] = VGND;
+    assign uio_oe[3] = VGND;
+    assign uio_oe[4] = VGND;
+    assign uio_oe[5] = VGND;
+    assign uio_oe[6] = VGND;
+    assign uio_oe[7] = VGND;
+    
+endmodule
